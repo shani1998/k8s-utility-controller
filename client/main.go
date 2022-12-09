9d5c582b7bcc650d15c48cb5e73fb7a95d6a2001
shani pathak <shani.pathak98@gmail.com>
2022-12-09 10:36:08 +0530

    added grpc metrics handler

--- client/main.go
+++ client/main.go
@@ -9,6 +9,7 @@ import (
 
 	pb "github.com/shani1998/k8s-utility-controller/proto"
 
+	grpcprometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -25,19 +26,35 @@ var (
 func main() {
 	flag.Parse()
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := connect(context.Background(), *addr)
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("unable to connect to server: %v", err)
 	}
 	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+	cl := pb.NewGreeterClient(conn)
+	r, err := cl.SayHello(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
+
 	log.Printf("Greeting: %s", r.GetMessage())
+}
+
+func connect(ctx context.Context, uri string) (*grpc.ClientConn, error) {
+	dialOpts := []grpc.DialOption{
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`), // client side load balancing
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.FailOnNonTempDialError(true),
+		grpc.WithBlock(),
+		grpc.WithUnaryInterceptor(grpcprometheus.UnaryClientInterceptor),
+	}
+	conn, err := grpc.DialContext(ctx, uri, dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
--- server/main.go
+++ server/main.go
@@ -4,8 +4,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"net"
+	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	pb "github.com/shani1998/k8s-utility-controller/proto"
 	log "github.com/sirupsen/logrus"
 )
@@ -21,12 +24,30 @@ type server struct {
 
 func main() {
 	flag.Parse()
+
+	//doneC := make(chan error)
+	//globalCtx, globalCancel := context.WithCancel(context.Background())
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := NewGRPCServer()
 	pb.RegisterGreeterServer(s, &server{})
+
+	go func() {
+		metricsMux := http.NewServeMux()
+		// After all your registrations, make sure all of the Prometheus metrics are initialized.
+		grpc_prometheus.Register(s)
+		// Register Prometheus metrics handler.
+		metricsMux.Handle("/metrics", promhttp.Handler())
+		address := net.JoinHostPort("", "9090")
+		log.Infof("Starting metric server at address [%s]", address)
+		if err := http.ListenAndServe(address, metricsMux); err != nil {
+			log.Errorf("Failed to serve metrics at [%s]: %v", address, err)
+		}
+	}()
+
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
